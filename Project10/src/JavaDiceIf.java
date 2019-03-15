94ad7b48482308c4ce6085fa1790ab4de65ad2fe
hyeongseobb <cherry2370@naver.com>
2019-03-15 18:26:11 +0900

    Signed-off-by: hyeongseobb <cherry2370@naver.com>

--- Project10/src/JavaDiceIf.java
+++ Project10/src/JavaDiceIf.java
@@ -4,21 +4,23 @@ public class JavaDiceIf {
 	public static void main (String[] args) {
 		
 		Random r = new Random();
-		int dice = r.nextInt(7);
+		int dice = r.nextInt(6);
 		
-		if(dice == 1) {
+		System.out.println(dice + 1);
+		
+		if(dice == 0) {
 			System.out.println("港港");
 		}
-		else if(dice == 2) {
+		else if(dice == 1) {
 			System.out.println("具克");
 		}
-		else if(dice == 3) {
+		else if(dice == 2) {
 			System.out.println("3");
 		}
-		else if(dice == 4) {
+		else if(dice == 3) {
 			System.out.println("4");
 		}
-		else if(dice == 5) {
+		else if(dice == 4) {
 			System.out.println("5");
 		}
 		else {
