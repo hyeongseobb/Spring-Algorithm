1e29726e5d74273344a794cf2ee3895fdc25853d
hyeongseobb <cherry2370@naver.com>
2019-03-21 17:58:37 +0900

    Signed-off-by: hyeongseobb <cherry2370@naver.com>

---
+++ Project11Star/src/JavaStar1.java
@@ -0,0 +1,56 @@
+
+public class JavaStar1 {
+	public static void main (String[] args) {
+		for(int i=0; i<4; i++){
+			for(int j=0; j<10-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<(2*i+1); k++) {
+				System.out.print("*");
+			}
+			System.out.println();		
+		}
+		
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<21-2*i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<4-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<13+2*i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i=0; i<4; i++) {
+			System.out.print(" ");
+			for(int j=0; j<6+i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<7-2*i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\r\r");
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5-i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}				
+	}
+}
+
